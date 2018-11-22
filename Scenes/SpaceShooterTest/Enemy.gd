d62d226d92413500e17fcedc21a46cafc903b88e
SGG300 <sergio.g.g300@gmail.com>
2018-11-22 22:35:48 +0100

    Base Enemy
    Enemy Basic

---
+++ Scenes/SpaceShooterTest/Enemy.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+export var velocity = Vector2()
+onready var size = get_node("Sprite").texture.get_size()
+
+func _ready():
+	set_process(true)
+	pass
+	
+func _process(delta):
+	translate(velocity * delta)
+	
+	if position.y >= get_viewport_rect().size.y + size.y:
+		queue_free()
+	pass
