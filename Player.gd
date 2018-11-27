929110073f1cd07d4254e5e39d1355900c21e450
Juanma Reyes <juanma.resp@gmail.com>
2018-11-27 19:37:38 +0100

    Change input mapping

--- Player.gd
+++ Player.gd
@@ -7,7 +7,8 @@ const GRAVITY = 40
 const ACCELERATION = 50
 const MAX_SPEED = 500
 const JUMP_HEIGHT = -850
-#const JUMP_HEIGHT = -2000
+const SUPER_JUMP_HEIGHT = -2500
+const DASH_DISTANCE = 1200
 const JETPACK_DOWN_SPEED = -220
 const JETPACK_HEIGHTDIFF_THRESHOLD = 10
 
@@ -50,10 +51,15 @@ func _physics_process(delta):
 		sprite.play(ANIM_IDLE)
 		motion.x = lerp(motion.x, 0, DAMPING_FLOOR if is_on_floor() else DAMPING_SKY)
 	
+	if Input.is_action_just_released("left_dash"):
+		motion.x = -DASH_DISTANCE
+	elif Input.is_action_just_released("right_dash"):
+		motion.x = DASH_DISTANCE
+	
 	if sprite.flip_h and projectile_pos.position.x > 0 or not sprite.flip_h and projectile_pos.position.x < 0:
 		projectile_pos.position.x *= -1
 	
-	if Input.is_action_just_pressed("ui_accept"):
+	if Input.is_action_just_pressed("shoot"):
 		shoot()
 	
 	var prev_falling_down = falling_down
@@ -63,11 +69,13 @@ func _physics_process(delta):
 		latest_height = self.position.y
 		
 	if is_on_floor():
-		if Input.is_action_just_pressed("ui_up"):
+		if Input.is_action_just_pressed("ui_accept"):
 			motion.y = JUMP_HEIGHT
+		elif Input.is_action_pressed("left_dash") and Input.is_action_pressed("right_dash"):
+			motion.y = SUPER_JUMP_HEIGHT
 	else:
 		sprite.play(ANIM_JUMP)
-		if falling_down and Input.is_action_pressed("ui_up"):
+		if falling_down and Input.is_action_pressed("ui_accept"):
 			motion.y = lerp(motion.y, JETPACK_DOWN_SPEED, DAMPING_FALLING)
 			jetpack_on = true
 		else:
