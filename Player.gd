a84702b2ce874d8f8fda36a9bbdfa67f99ea592f
Juanma Reyes <juanma.resp@gmail.com>
2018-11-30 20:01:13 +0100

    Add dash animation

--- Player.gd
+++ Player.gd
@@ -24,6 +24,7 @@ const JETPACK_ENERGY_FOR_DASH = 0.5
 const JETPACK_REGEN_SPEED = 0.3 # points per sec
 
 const DASHING_TIME = 0.2 # sec
+const ANIM_TIME_HIT = 0.3 # sec
 
 const ANIM_IDLE = "Idle"
 const ANIM_WALK = "Walk"
@@ -38,6 +39,8 @@ onready var sprite = $AnimatedSprite
 onready var collision_polygon = $CollisionPolygon2D
 onready var projectile_pos = $ProjectilePosition
 onready var jetpack_particles = $JetpackParticles
+onready var dash_effect_right_particles = $DashEffectRightParticles
+onready var dash_effect_left_particles = $DashEffectLeftParticles
 onready var invulnerable_after_hit_timer = $InvulnerableAfterHitTimer
 
 var motion = Vector2()
@@ -57,6 +60,9 @@ var is_dead = false
 var dashing_left = false
 var dashing_right = false
 var dashing_elapsed_time = 0
+
+var just_hit = false
+var anim_time_hit_elapsed_time = 0
 
 var elapsed_time = 0
 
@@ -83,11 +89,19 @@ func _physics_process(delta):
 	if Input.is_action_just_released("left_dash"):
 		if not just_superjumped_l_pressed and jetpack_energy >= JETPACK_ENERGY_FOR_DASH:
 			dashing_left = true
+			if sprite.flip_h:
+				dash_effect_left_particles.emitting = true
+			else:
+				dash_effect_right_particles.emitting = true
 			spend_energy(JETPACK_ENERGY_FOR_DASH)
 		just_superjumped_l_pressed = false
 	elif Input.is_action_just_released("right_dash"):
 		if not just_superjumped_r_pressed and jetpack_energy >= JETPACK_ENERGY_FOR_DASH:
 			dashing_right = true
+			if sprite.flip_h:
+				dash_effect_left_particles.emitting = true
+			else:
+				dash_effect_right_particles.emitting = true
 			spend_energy(JETPACK_ENERGY_FOR_DASH)
 		just_superjumped_r_pressed = false
 	
@@ -106,6 +120,8 @@ func _physics_process(delta):
 			dashing_elapsed_time = 0
 			dashing_left = false
 			dashing_right = false
+			dash_effect_left_particles.emitting = false
+			dash_effect_right_particles.emitting = false
 	
 	if sprite.flip_h and projectile_pos.position.x > 0 or not sprite.flip_h and projectile_pos.position.x < 0:
 		projectile_pos.position.x *= -1
@@ -144,6 +160,14 @@ func _physics_process(delta):
 			if not superjumping:
 				jetpack_particles.emitting = false
 	
+	if just_hit:
+		anim_time_hit_elapsed_time += delta
+		if anim_time_hit_elapsed_time > ANIM_TIME_HIT:
+			anim_time_hit_elapsed_time = 0
+			just_hit = false
+		else:
+			sprite.play(ANIM_HIT)
+	
 	if superjumping:
 		sprite.play(ANIM_JUMP)
 	
@@ -173,6 +197,7 @@ func damage():
 			invulnerable = true
 			modulate = Color(1.0, 1.0, 1.0, 0.5)
 			invulnerable_after_hit_timer.start()
+			just_hit = true
 		else:
 			var explosion = Controller.explosion_scene.instance()
 			explosion.position = position
